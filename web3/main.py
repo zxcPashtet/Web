153fd84c39b780eed894b66077533b58f7a9d5ad
Pashtet <pavel.pivikov2008@yandex.ru>
2025-03-02 00:41:06 +0300

    Registration form

--- web3/main.py
+++ web3/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect
 from Data import db_session
 from Data.users import User
 from Data.jobs import Jobs
+from form.register import RegisterForm
 import datetime
 
 app = Flask(__name__)
@@ -73,6 +74,30 @@ def index():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     return render_template('table_jobs.html', jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form, message='Пароли не совпадают')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form, message='Такой пользователь уже есть')
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
 
 
 def main():
